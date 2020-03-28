b7d6ec5183c54d0734cf055846d5b1b5e7d927dc
Adarsh Singh <2001adarshsingh@gmail.com>
2020-03-28 09:25:55 +0530

    Updated

--- NavigationalDrawerPractice/app/src/main/java/com/adarsh/navigationaldrawerpractice/Navigational.java
+++ NavigationalDrawerPractice/app/src/main/java/com/adarsh/navigationaldrawerpractice/Navigational.java
@@ -24,6 +24,7 @@ public class Navigational extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigational);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
