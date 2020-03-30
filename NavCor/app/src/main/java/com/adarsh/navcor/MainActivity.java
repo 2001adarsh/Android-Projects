04b6795657bea12744c718999a091a7fcf15010a
Adarsh Singh <2001adarshsingh@gmail.com>
2020-03-30 21:00:28 +0530

    App completed

--- NavCor/app/src/main/java/com/adarsh/navcor/MainActivity.java
+++ NavCor/app/src/main/java/com/adarsh/navcor/MainActivity.java
@@ -1,35 +1,38 @@
  package com.adarsh.navcor;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.adarsh.navcor.ui.HomeFragment;
+import com.adarsh.navcor.ui.cartFragment;
+import com.adarsh.navcor.ui.settingFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
- public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private AppBarConfiguration mAppBarConfiguration;
-
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-//  This app:headerLayout inflates the specified layout into the header automatically. This can alternatively be done at runtime with:
-//
-//  // Lookup navigation view
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nvView);
-//  // Inflate the header view at runtime
-//        View headerLayout = navigationView.inflateHeaderView(R.layout.nav_header);
-//  // We can now look up items within the header if needed
-//        ImageView ivHeaderPhoto = headerLayout.findViewById(R.id.imageView);
 
         Toolbar toolbar = findViewById(R.id.toolb);
         setSupportActionBar(toolbar);
@@ -43,9 +46,86 @@ import com.google.android.material.snackbar.Snackbar;
             }
         });
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+         drawer = findViewById(R.id.drawer_layout);
+         navigationView = findViewById(R.id.nav_view);
 
+         navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //This is when device is rotated, it doesn't looses its fragment.
+        if(savedInstanceState == null) {
+            //When we start the activity, we do not want to show an empty fragment, so--
+            getSupportFragmentManager().beginTransaction().replace(R.id.Frame_layout,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+        /*
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home,R.id.nav_gallery,
+                R.id.nav_slideshow).setDrawerLayout(drawer).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController,mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        */
 
     }
-}
+
+     @Override
+     public void onBackPressed() {
+         if (drawer.isDrawerOpen(GravityCompat.START)) {
+             drawer.closeDrawer(GravityCompat.START);
+         } else {
+             super.onBackPressed();
+         }
+     }
+
+     @Override
+     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Frame_layout,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_gallery:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Frame_layout,
+                        new cartFragment()).commit();
+                break;
+            case R.id.nav_slideshow:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Frame_layout,
+                        new settingFragment()).commit();
+                break;
+            //case R.id.something: -- For showing a Toast message
+            // Toast();
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+         //here return false means no item is being selected. So we return true;
+     }
+
+
+
+
+    /*
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigational, menu);
+        return true;
+        // return super.onCreateOptionsMenu(menu);
+     }
+
+     @Override
+     public boolean onSupportNavigateUp() {
+         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                 || super.onSupportNavigateUp();
+
+     }
+     */
+ }
