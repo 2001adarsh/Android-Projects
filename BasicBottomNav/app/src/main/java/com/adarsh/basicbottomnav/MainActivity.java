15f28803e83ef991ec623cfa67162484ff144c25
Adarsh Singh <2001adarshsingh@gmail.com>
2020-04-11 12:17:58 +0530

    updated

--- BasicBottomNav/app/src/main/java/com/adarsh/basicbottomnav/MainActivity.java
+++ BasicBottomNav/app/src/main/java/com/adarsh/basicbottomnav/MainActivity.java
@@ -34,31 +34,10 @@ public class MainActivity extends AppCompatActivity {
         favoriteFragment = new FavoriteFragment();
         settingsFragment = new SettingsFragment();
 
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, homeFragment).commit();
 
-                switch (item.getItemId()) {
-                    case R.id.bot_home:
-                        bottomNav.setItemBackgroundResource(R.color.colorPrimary);
-                        setFragment(homeFragment);
-                        return true;
+        bottomNav.setOnNavigationItemSelectedListener(navlistner);
 
-                    case R.id.nav_set:
-                        bottomNav.setItemBackgroundResource(R.color.colorAccent);
-                        setFragment(settingsFragment);
-                        return true;
-
-                    case R.id.nav_fav:
-                        bottomNav.setItemBackgroundResource(R.color.colorPrimaryDark);
-                        setFragment(favoriteFragment);
-                        return true;
-
-                    default:
-                    return false;
-                }
-            }
-        });
     }
 
     public void setFragment(Fragment fragment){
@@ -67,4 +46,27 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.bot_home:
+                            bottomNav.setItemBackgroundResource(R.color.colorPrimary);
+                            setFragment(homeFragment);
+                            break;
+
+                        case R.id.nav_set:
+                            bottomNav.setItemBackgroundResource(R.color.colorAccent);
+                            setFragment(settingsFragment);
+                            break;
+
+                        case R.id.nav_fav:
+                            bottomNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                            setFragment(favoriteFragment);
+                            break;
+                    }
+                    return true;
+                }
+            };
 }
--- customBottomNav/app/src/main/java/com/adarsh/custombottomnav/MainActivity.java
+++ customBottomNav/app/src/main/java/com/adarsh/custombottomnav/MainActivity.java
@@ -14,7 +14,7 @@ import com.adarsh.custombottomnav.ui.ProfileFragment;
 import com.adarsh.custombottomnav.ui.SearchFragment;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements ChipNavigationBar.OnItemSelectedListener {
     private static final String TAG = "FragmentT";
     ChipNavigationBar navigationBar;
 
@@ -27,38 +27,40 @@ public class MainActivity extends AppCompatActivity {
 
         if(savedInstanceState==null)
         {
+            navigationBar.setItemSelected(R.id.nav_home,true);
             FragmentManager fm = getSupportFragmentManager();
             fm.beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
+
+        }
+        navigationBar.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(int i) {
+        Fragment fragment = null;
+        switch (i){
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_cart:
+                fragment = new CartFragment();
+                break;
+            case R.id.nav_search:
+                fragment = new SearchFragment();
+                break;
+            case R.id.nav_profile:
+                fragment = new ProfileFragment();
+                break;
         }
 
-        navigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(int i) {
-                Fragment fragment = null;
-                switch (i){
-                    case R.id.nav_home:
-                        fragment = new HomeFragment();
-                        break;
-                    case R.id.nav_cart:
-                        fragment = new CartFragment();
-                        break;
-                    case R.id.nav_search:
-                        fragment = new SearchFragment();
-                        break;
-                    case R.id.nav_profile:
-                        fragment = new ProfileFragment();
-                        break;
-                }
-
-                if(fragment!= null)
-                {
-                    FragmentManager fm = getSupportFragmentManager();
-                    fm.beginTransaction().replace(R.id.main_frame, fragment).commit();
-                }
-                else{
-                    Log.e(TAG, "Error in creating fragment");
-                }
-            }
-        });
+        if(fragment!= null)
+        {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.main_frame, fragment).commit();
+        }
+        else{
+            Log.e(TAG, "Error in creating fragment");
+        }
     }
 }
